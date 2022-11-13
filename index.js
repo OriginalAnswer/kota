c54b5ee0eb93220c1f3b96b705009cd8b6572384
OriginalAnswer <akiseonair@gmail.com>
2022-11-14 00:31:32 +0900

    1114

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
-const timeKR = document.getElementById('s1-time-kr-span');
-function getTimeKR(){
+// const mArr = new Array('Jan','Feb','Mar','Apr','May','Jun','July','Aug','Sep','Oct','11','Dec');
+//로컬타임 하나
+const tt = document.getElementById('main-s1-thistime-time');
+function getThistime(){
     const date = new Date();
     const h = String(date.getHours()).padStart(2,"0");
     const m = String(date.getMinutes()).padStart(2,"0");
-    const s = String(date.getSeconds()).padStart(2,"0");
-    timeKR.innerText = `${h}:${m}`;
+    tt.innerText = `${h}:${m}`;
 };
-const timeKK = document.getElementById('s1-time-kk-span');
-function getTimeKK(){
-    var kota_offset = 4*60;
-    const date = new Date();
-    date.setMinutes(date.getMinutes()+kota_offset);
-    const h = String(date.getHours()).padStart(2,"0");
-    const m = String(date.getMinutes()).padStart(2,"0");
-    // const s = String(date.getSeconds()).padStart(2,"0");
-    timeKK.innerText = `${h}:${m}`;
-};
-// 
-const today = document.getElementById('s1-date-span');
-const mArr = new Array('Jan','Feb','Mar','Apr','May','Jun','July','Aug','Sep','Oct','11','Dec');
-function getDate(){
+setInterval(getThistime, 1000);
+
+const mainDate = document.getElementById('main-s1-thistime-date');
+const mainDay = document.getElementById('main-s1-thistime-day');
+function getDateMain(){
     const date = new Date();
     const m = date.getMonth();
     const d = String(date.getDate());
     today.innerText = `${mArr[m]}/${d}`;
 };
-getDate();
-setInterval(getTimeKK, 1000);
-setInterval(getTimeKR, 1000);
+getDateMain();
