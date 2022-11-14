21a1a900aa9f1d851fc67bc47815a07bc9a13ff0
OriginalAnswer <akiseonair@gmail.com>
2022-11-15 02:04:30 +0900

    1115

--- indexdday.js
+++ indexdday.js
@@ -1,6 +1,27 @@
-const thisdate = document.getElementById('main-s1-thistime-date');
-const dd = document.getElementById('main-s2-dday-span'); //D-day
+function setDday(){
+    const htmlDday = document.getElementById('dday')
+    // const startdate = 20221117;
+    // const sy = 2022;
+    // const sm = 11;
+    const sd = 17;
 
-function dday(){
+    const thisdate = new Date();
+    // const ty = thisdate.getFullYear();
+    // const tm = thisdate.getMonth()+1;
+    const td = thisdate.getDate();
 
-}
+    // const ry = ty - sy;
+    // const rm = tm - sm;
+    const rd = td - sd;
+
+    let dd = rd;
+    if(rd >= 0){
+        dd = rd + 1;
+    }
+    // console.log(ry);
+    // console.log(rm);
+    // console.log(rd);
+    console.log(dd);
+    htmlDday.innerText = `DAY${dd}`;
+};
+setDday();
