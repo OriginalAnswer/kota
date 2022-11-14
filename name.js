d852dc5d95134a166e1fb8271366e98ceb3abc42
OriginalAnswer <akiseonair@gmail.com>
2022-11-15 08:02:15 +0900

    1115

--- name.js
+++ name.js
@@ -1,34 +1,41 @@
 const NAME = 'name';
 const savedName = localStorage.getItem(NAME);
 const scd = document.getElementById('main-s2-schadule');
+const scdFtr = document.getElementById('main-s2-footer');
 const n = document.getElementById('name');
 
 function Name1(){
     localStorage.setItem(NAME, "김도혁");
     n.style.display = 'none'
     scd.classList.remove('dp-none');
+    scdFtr.classList.remove('dp-none');
 }
 function Name2(){
     localStorage.setItem(NAME, "한강익");
     n.style.display = 'none'
     scd.classList.remove('dp-none');
+    scdFtr.classList.remove('dp-none');
 }
 function Name3(){
     localStorage.setItem(NAME, "박정진");
     n.style.display = 'none'
     scd.classList.remove('dp-none');
+    scdFtr.classList.remove('dp-none');
 }
 function Name4(){
     localStorage.setItem(NAME, "김우섭");
     n.style.display = 'none'
     scd.classList.remove('dp-none');
+    scdFtr.classList.remove('dp-none');
 }
 function Name5(){
     localStorage.setItem(NAME, "백종훈");
     n.style.display = 'none'
     scd.classList.remove('dp-none');
+    scdFtr.classList.remove('dp-none');
 }
 if(savedName !== null){
     n.style.display = 'none';
     scd.classList.remove('dp-none');
+    scdFtr.classList.remove('dp-none');
 }
